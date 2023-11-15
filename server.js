446f41b62d41c8d45338fcc449cf78dcbe3e8187
zerolegacy6 <einstingotimjunior25@gmail.com>
2023-11-15 16:01:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-//this is where your express sever is created and managed
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-//do not forget to import the necessary modules,
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//do not forget to connect to your mongoDb
+// MongoDB Connection
+const dbURI = 'mongodb+srv://otimjunior:GILLIANZ@cluster0.kqaubaq.mongodb.net/?retryWrites=true&w=majority'; // Replace with your actual MongoDB URI
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Check if the connection is successful
+const db = mongoose.connection;
+db.on('error', (error) => console.error('MongoDB connection error:', error));
+db.once('open', () => console.log('Connected to MongoDB'));
 
+// Your API routes go here
 
-//do not forget to set up routes to serve out the html file
-
-// Do not forget to handle your form posts for the relevant forms
-
-// Do not forget to handle form gets for the necessary data to display on the user interface.
-
-
-//Do not forget to catch all the erros that may arise during the process of parsing data from one form to another.
-
-// do not forget to create a port for your application and serving out the appliacation via the port created 
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at port http://localhost:${port}/`)
+});
