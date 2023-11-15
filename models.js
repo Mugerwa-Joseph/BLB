ae906e546d7e4496be3837abb18fb15a37905fe7
hanningt <93286519+hanningt@users.noreply.github.com>
2023-11-15 17:43:41 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,14 +1,21 @@
 //Remember to import in mongoose
-
+const mongoose =require('mongoose')
 
 //create the citizen schema here with the appropriate data types
+const citizenSchema = new mongoose.Schema({
+  name:String,
+  dob:String,
+  fathername:String,
+  mothername:String,
+  gender:String,
+  bloodGroup:String
+})
 
 
 //create the title schema here wit the right fields
 
-
 //convert the schemas into models from here
-
+const citizenModel = mongoose.model('models', citizenSchema)
 
 //export the models as modules from here.
-
+module.exports = citizenModel
