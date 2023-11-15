5fc1b80c6838003db96dfac00d343b61b8f458b5
zerolegacy6 <einstingotimjunior25@gmail.com>
2023-11-15 16:00:30 +0300

    Update models.js

--- models.js
+++ models.js
@@ -1,14 +1,31 @@
-//Remember to import in mongoose
+// models.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://otimjunior:GILLIANZ@cluster0.kqaubaq.mongodb.net/BLB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
-//create the citizen schema here with the appropriate data types
+// Define multiple models
+const CitizenModel = mongoose.model('citizen', {
+  Name: String,
+  dateOfBirth: String, 
+  fatherName: String,
+  motherName: String, 
+  gender: String, 
+  bloodGroup: String 
+});
 
+const TitleModel = mongoose.model('title', {
+    ownerName: String,
+    location: String, 
+    size: Number,
+    coordinates: String,
+    titleNumber: String,
+    satellitePhoto: String 
+});
 
-//create the title schema here wit the right fields
-
-
-//convert the schemas into models from here
-
-
-//export the models as modules from here.
-
+// Export the models
+module.exports = { CitizenModel, TitleModel };
