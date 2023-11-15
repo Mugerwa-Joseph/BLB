7ab76d45b81c5b740ba9f86dc707cbeccb5cf90b
zerolegacy6 <einstingotimjunior25@gmail.com>
2023-11-15 16:51:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { CitizenModel, TitleModel } = require('./models'); // Adjust the path accordingly
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,64 +10,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Connecting to the MongoDB
-mongoose.connect('mongodb+srv://otimjunior:GILLIANZ@cluster0.kqaubaq.mongodb.net/Orders?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect('mongodb+srv://otimjunior:GILLIANZ@cluster0.kqaubaq.mongodb.net/BLB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection Error'));
-db.once('open', function (){
-    console.log('Connected to Our db, BLB');
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
 });
-
-// Define Mongoose schema and model for Citizen
-const citizenSchema = new mongoose.Schema({
-  name: String,
-  dateOfBirth: String,
-  fatherName: String,
-  motherName: String,
-  gender: String,
-  bloodGroup: String,
-});
-
-const Citizen = mongoose.model('Citizen', citizenSchema);
-
-// Define Mongoose schema and model for Title
-const titleSchema = new mongoose.Schema({
-  ownerName: String,
-  location: String,
-  size: Number,
-  coordinates: String,
-  titleNumber: String,
-  satellitePhoto: String,
-});
-
-const Title = mongoose.model('Title', titleSchema);
 
 // Handling Citizen Registration Form submission
 app.post('/citizen', async (req, res) => {
-  try {
-    const citizenData = new Citizen(req.body);
-    await citizenData.save();
-    res.status(201).send('Citizen data submitted successfully!');
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+    try {
+        const citizenData = new CitizenModel(req.body);
+        await citizenData.save();
+        res.status(201).send('Citizen data submitted successfully!');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
 // Handling Title Registration Form submission
 app.post('/title', async (req, res) => {
-  try {
-    const titleData = new Title(req.body);
-    await titleData.save();
-    res.status(201).send('Title data submitted successfully!');
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
+    try {
+        const titleData = new TitleModel(req.body);
+        await titleData.save();
+        res.status(201).send('Title data submitted successfully!');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
 });
 
+// Start the server
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at port http://localhost:${port}/`)
 });
